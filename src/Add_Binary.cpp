335495c4f1b8b083f93edccf61757aebae3238c2
rogerAce <renlong.dut@icloud.com>
2014-10-27 14:50:58 +0800

    solution for add binary

---
+++ src/Add_Binary.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result;
+        const size_t n = a.size() > b.size() ? a.size() : b.size();
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        int carry = 0;
+        for (size_t i = 0; i < n; i++) {
+        const int ai = i < a.size() ? a[i] - '0' : 0;
+        const int bi = i < b.size() ? b[i] - '0' : 0;
+        const int val = (ai + bi + carry) % 2;
+        carry = (ai + bi + carry) / 2;
+        result.insert(result.begin(), val + '0');
+        }
+        if (carry == 1) {
+        result.insert(result.begin(), '1');
+        }
+        return result;
+    }
+};
+
+int main()
+{
+	Solution s;
+	string str1("11");
+	string str2("1");
+	string ss = s.addBinary(str1,str2);
+	cout<<s<<endl;
+	system("pause");
+	return 0;
+}
